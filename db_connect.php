4dd37e71d7664ed5d17db1a681f42d7a0d2b66f4
Jansus <nik.janos04@gmail.com>
2025-05-11 16:29:59 +0200

    db_connection spremenjen

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,18 @@
+
 <?php
+////"Server=eknjiznica-db.database.windows.net;Database=E-knjiznica;User Id=eknjiznica-sa;Password=yourStrong(!)Password;TrustServerCertificate=true", ta še dela za php:
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO(
-        "sqlsrv:server = tcp:{$_ENV['DB_HOST']},1433; Database = {$_ENV['DB_NAME']}",
-        $_ENV['DB_USER'],
-        $_ENV['DB_PASSWORD']
-    );
+    $conn = new PDO("sqlsrv:server = tcp:eknjiznica-db.database.windows.net,1433; Database = E-knjiznica", "eknjiznica-sa", "yourStrong(!)Password");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
 }
-?>
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "eknjiznica-sa", "pwd" => "{your_password_here}", "Database" => "E-knjiznica", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:eknjiznica-db.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?> 
