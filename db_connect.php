c4f3c17903907f3b9e2f8770ceac17ff29362e42
Jansus <nik.janos04@gmail.com>
2025-05-11 16:34:05 +0200

    changed index.html to index.php for azure

--- db_connect.php
+++ db_connect.php
@@ -15,4 +15,4 @@ catch (PDOException $e) {
 $connectionInfo = array("UID" => "eknjiznica-sa", "pwd" => "yourStrong(!)Password", "Database" => "E-knjiznica", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:eknjiznica-db.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-?> 
+?>
